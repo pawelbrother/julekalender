2d4a7fd35c24a655abdbca4f6e8393d7359481d1
pawelmr <paw3lrod@gmail.com>
2024-11-28 15:34:54 +0100

    Endret til at den viser riktig dato

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,7 @@ export default function Home() {
 
   const [showBrus, setShowBrus] = useState(false);
 
-  // const currentDate = new Date().toISOString().split('T')[0]; // Get current date in 'YYYY-MM-DD' format
-  const currentDate = 11;
+  const currentDate = new Date().getDate(); // Get current date in 'YYYY-MM-DD' format
   const imageSrc = `/${currentDate}.png`;
 
   return (
